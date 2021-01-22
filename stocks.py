91b5061fe193b0acebf90fde7ef3af00f077b8cf
Magnus Fulton <magnusjackfulton@gmail.com>
2021-01-21 18:24:30 -0800

    [Python 2 v0.1] PSG day 5

--- stocks.py
+++ stocks.py
@@ -1,36 +1,79 @@
 import easygui
 from time import sleep
 import random
-print "Welcome to StockSimulator!"
-print "Starting..."
-gameisreset = 0 #if reset it would be 1
-print "Setting bank variables"
-BankT = 10000
-BankD = 0
-print "Bank variables set!"
-print "Adding Stocks"
-SGD = random.randint(1, 50)
-DAHJ = random.randint(20, 105)
-MFHG = random.randint(1, 30)
 
-SGDowned = 0
-DAHJowned = 0
-MFHGowned = 0
+def startup():
+    print "Welcome to StockSimulator!"
+    print "Starting..."
+    gameisreset = 0 #if reset it would be 1
+    print "Setting bank variables"
+    BankT = 10000
+    BankD = 0
+    print "Bank variables set!"
+    print "Adding Stocks"
+    SGD = random.randint(1, 50)
+    DAHJ = random.randint(20, 105)
+    MFHG = random.randint(1, 30)
 
-print "Adding Game Bonus counter"
-daystobonus = 10
+    SGDowned = 0
+    DAHJowned = 0
+    MFHGowned = 0
 
-print "Adding day counter!"
-days = 1
-print "Adding stock averages variables"
+    print "Adding Game Bonus counter"
+    daystobonus = 10
 
-SGDy = SGD
-DAHJy = DAHJ
-MFHGy = MFHG
+    print "Adding day counter!"
+    days = 1
+    print "Adding stock averages variables"
 
-SGDa = SGDy / days
-DAHJa = DAHJy / days
-MFHGa = MFHGy / days
+    SGDy = SGD
+    DAHJy = DAHJ
+    MFHGy = MFHG
+
+    SGDa = SGDy / days
+    DAHJa = DAHJy / days
+    MFHGa = MFHGy / days
+
+import_save = easygui.buttonbox("A save file may have been detected. Would you like to import it, or reset game progress? If no save file is present, the game will crash! To fix this click reset and in the main menu, select save.",
+                                choices = ["Load", "Reset"])
+if import_save == "Load":
+    
+    from variables import *
+else:
+    print "Welcome to StockSimulator!"
+    print "Starting..."
+    gameisreset = 0 #if reset it would be 1
+    print "Setting bank variables"
+    BankT = 10000
+    BankD = 0
+    print "Bank variables set!"
+    print "Adding Stocks"
+    SGD = random.randint(1, 50)
+    DAHJ = random.randint(20, 105)
+    MFHG = random.randint(1, 30)
+
+    SGDowned = 0
+    DAHJowned = 0
+    MFHGowned = 0
+
+    print "Adding Game Bonus counter"
+    daystobonus = 10
+
+    print "Adding day counter!"
+    days = 1
+    print "Adding stock averages variables"
+
+    SGDy = SGD
+    DAHJy = DAHJ
+    MFHGy = MFHG
+
+    SGDa = SGDy / days
+    DAHJa = DAHJy / days
+    MFHGa = MFHGy / days
+
+
+
+
 #***************************
 #random.randint breaks if you use decimals
 #This might be a good thing, as I can use 1.4 multiplier, then divide, to get
@@ -38,6 +81,9 @@ MFHGa = MFHGy / days
 #***************************
 print "Stock values added!"
 print "The stock starter values are:","SGD:", SGD,"|","DAHJ:", DAHJ,"|","MFHG", MFHG
+
+
+
 print "Setting game functions"
 
 #***************************
@@ -48,6 +94,10 @@ print "Setting game functions"
 #When calling a varible in a function, use global then the varible name. No idea why it works, it just does!
 
 def advance():
+
+    #Save first!
+    S()
+    
     global BankT
     global days
     days = days + 1
@@ -77,19 +127,23 @@ def advance():
                                       choices = ['Accept', 'Pass'] )
           
         if choice == "Pass":
-            print "You declined the 10 day bonus offer."
-        else:
-                chance = random.randint(1, 2)
-        if chance == 1:
+            easygui.msgbox("You declined the 10 day bonus!")
+
+        if choice == "Accept":
+            chance = random.randint(0, 20)
+            if chance < 9:
                 SGDowned == SGDowned + 57
                 SGD = SGD*2
                 BankT = BankT + 5,000
                 print "You just won the Ten Day Bonus!"
                 BB()
-        else:
+            else:
                 BankT = BankT / 99
                 print "You just lost the Ten Day Bonus."
                 BB()
+        
+            
+                
 
 
 
@@ -103,6 +157,10 @@ def advance():
     global DAHJy
     global MFHGy
 
+    SGDy = SGD + SGDy
+    DAHJy = DAHJ + DAHJy
+    MFHGy = MFHG + MFHGy
+
     SGDa = SGDy / days
     DAHJa = DAHJy / days
     MFHGa = MFHGy / days
@@ -115,6 +173,10 @@ def advance():
     if BankT == 0:
         print "================================================================================"
         print "Game over! All money was lost! Save will be reset!"
+        with open('variables.py','w') as v:
+            #Eracicate old file:
+            v.seek(0)
+            v.truncate()
         print "Warning! Game will now throw a 0 / 0 error. Please ignore this error and start program again."
         print 0 / 0
       
@@ -298,6 +360,70 @@ def Controls ():
 
 #Controls work fine, however as of day 4, they become irrelevant.
 """
+
+def Save():
+        with open('variables.py','w') as v:
+            #Eracicate old file:
+            v.seek(0)
+            v.truncate()
+            #Save variables as string
+            gameisresets = "gameisreset = " + str(gameisreset) 
+            BankTs = "BankT = " + str(BankT)
+            BankDs = "BankD = " + str(BankD)
+            SGDs = "SGD = " + str(SGD)
+            DAHJs = "DAHJ = " + str(DAHJ)
+            MFHGs = "MFHG = " + str(MFHG)
+
+            SGDowneds = "SGDowned = " + str(SGDowned)
+            DAHJowneds = "DAHJowned = " + str(DAHJowned)
+            MFHGowneds = "MFHGowned = " + str(MFHGowned)
+
+            SGDys = "SGDy = " + str(SGDy)
+            DAHJys = "DAHJy = " + str(DAHJy)
+            MFHGys = "MFHGy = " + str(MFHGy)
+
+            SGDas = "SGDa = " + str(SGDa)
+            DAHJas = "DAHJa = " + str(DAHJa)
+            MFHGas = "MFHGa = " + str(MFHGa)
+
+            dayss = "days = " + str(days)
+            daystobonuss = "daystobonus = " + str(daystobonus)
+            #Save stringed variables
+            v.write(gameisresets)
+            v.write('\n')
+            v.write(BankTs)
+            v.write('\n')
+            v.write(BankDs)
+            v.write('\n')
+            v.write(SGDs)
+            v.write('\n')
+            v.write(DAHJs)
+            v.write('\n')
+            v.write(MFHGs)
+            v.write('\n')
+            v.write(SGDowneds)
+            v.write('\n')
+            v.write(DAHJowneds)
+            v.write('\n')
+            v.write(MFHGowneds)
+            v.write('\n')
+            v.write(SGDys)
+            v.write('\n')
+            v.write(DAHJys)
+            v.write('\n')
+            v.write(MFHGys)
+            v.write('\n')
+            v.write(SGDas)
+            v.write('\n')
+            v.write(DAHJas)
+            v.write('\n')
+            v.write(MFHGas)
+            v.write('\n')
+            v.write(dayss)
+            v.write('\n')
+            v.write(daystobonuss)
+
+
 #***************************
 #Abbrievations
 #***************************
@@ -327,6 +453,12 @@ def DS():
 #BankBalance
 def BB():
     BankBalance()
+
+#Save
+
+def S():
+    Save()
+
 """
 #Controls
 def C():
@@ -424,6 +556,7 @@ while 1 == 1:
 
     if choice1 == "Advance":
         A()
+        
         intro = "SGD:", SGD,"|","DAHJ:", DAHJ,"|","MFHG:", MFHG
         easygui.msgbox(intro, "Today's stock value's are:")
 
@@ -433,8 +566,8 @@ while 1 == 1:
         choice2 = easygui.buttonbox("What would you like to view?",
                                     choices = ["Stocks", "Bank balance", "Controls"])
         if choice2 == "Stocks":
-            easygui.msgbox("Stock information will now be displayed. In 10 seconds, the main window will reopen.", "Stock Information")
             DS()
+            easygui.msgbox("Stock information will now be displayed. In 10 seconds, the main window will reopen. (This window need to be closed in order for countdown to begin)", "Stock Information")
             sleep(10)
 
         if choice2 == "Bank balance":
@@ -446,9 +579,15 @@ while 1 == 1:
             easygui.msgbox("Controls\n \nThis game has four main controls: Purchasing, Selling, Advancing, and displaying this list.\n \nPurchasing\n \nTo purchase a stock, select the [Purchase stock] button, select the stock you want to purchase and enter the number of stocks you want. Please note that entering an incorrect format, any besides numbers will crash the program. Additionally, enter only whole numbers; entering a number like 7.4 will only purchase 7 stocks.\n \nSelling\n \nSelling stocks works in a similar manner, select [Sell stocks], select the stock you want to sell. Additionally, enter only whole numbers; entering a number like 7.4 will only purchase 7 stocks.\n \nAdvancing\n \nTo move forward one day, select [Advance]. The new stock prices will display. You cannot undo a advance.\n \nDisplay controls\n \nTo find out more information on vaious things, like stocks and your bank balance, click on those catagories. The window will close and information will be displayed. After 10 seconds, the window will reopen and you can continue purchasing and selling stocks. Additionally, clicking on controls will display this list.","Controls","Next")
             easygui.msgbox("There are currently 3 stocks available for purchasing. These stocks are:\n \nSGD\n \nDAHJ\n \nMFHG.\n \nMFHG and SGD are coded to have smaller variations in price whereas DAHJ will tend to have larger variations. This won't always be true, but it is typically present.","Stocks","Next")
             easygui.msgbox("You can find a more in-depth discription of what the program is doing in the IDLE shell.","Behind the scenes","Next")
-            easygui.msgbox("10 day bonus\n \nThe 10 day bonus is a randomly selected prize which are acquired every 10 in-game days. 10 day bonus have good rewards, but can also cause monotary downfalls. If a 10 day bonus goes in your favor, you will win amazing rewards, but if it doesn't, the consequences can be dire!. The better the reward the greater the risk!","10 day bonus")
+            easygui.msgbox("10 day bonus\n \nThe 10 day bonus is a randomly selected prize which are acquired every 10 in-game days. 10 day bonus have good rewards, but can also cause monotary downfalls. If a 10 day bonus goes in your favor, you will win amazing rewards, but if it doesn't, the consequences can be dire!. The better the reward the greater the risk!","10 day bonus","Next")
+            easygui.msgbox("This game has saving functionality. You can save in multiple ways. First, on the main screen, you can select [save] to write all variables to the [ variables.py ]. second, you can select [Save and exit] to write all variables to [ variables.py ]. And third, you can advance, which saves by default.","Saving")
 
     #Save
+    if choice1 == "Save":
+        S()
+        easygui.msgbox("Saved game to [ variables.py ]")
+            
+        """
     if choice1 == "Save":
         with open('variables.py','w') as v:
             #Eracicate old file:
@@ -508,7 +647,7 @@ while 1 == 1:
             v.write('\n')
             v.write(daystobonuss)
             
-            
+            """
             
             
             
@@ -519,11 +658,15 @@ while 1 == 1:
 
     if choice1 == "Exit":
         choice2 = easygui.buttonbox("How would you like to exit?",
-                                    choices = ["Exit without saving", "Exit and save", "No exit"])
+                                    choices = ["Exit without saving", "Exit and save", "Clear Save", "No exit"])
         if choice2 == "Exit without saving":
-            choice3 = easygui.buttonbox("Are you sure?",
+            choice3 = easygui.buttonbox("Are you sure?\nThis action will clear the save file!",
                                         choices = ["Yes","No"])
             if choice3 == "Yes":
+                with open('variables.py','w') as v:
+                    #Eracicate old file:
+                    v.seek(0)
+                    v.truncate()
                 print "Game exited!"
                 break
                                     
@@ -531,7 +674,17 @@ while 1 == 1:
             choice3 = easygui.buttonbox("Are you sure?",
                                         choices = ["Yes","No"])
             if choice3 == "Yes":
-                print "Unable to save. Functionallity lacking."
+                S()
                 print "Game exited!"
                 break
+
+        if choice2 == "Clear Save":
+            choice3 = easygui.buttonbox("Warning! Clearing the save cannot be undone!\nAre you sure you want to do this?",
+                                        choices = ["Yes, clear the save file.", "No! Do not clear the save file!"])
+            if choice3 == "Yes, clear the save file.":
+                gameisreset = 1
+                with open('variables.py','w') as v:
+                    #Eracicate old file:
+                    v.seek(0)
+                    v.truncate()
                                     
